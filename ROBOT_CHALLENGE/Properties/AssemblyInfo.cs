de9ee048d8a1fdeedf84dd60fe46defb4d13bd47
Li Chai <jxcharlie1991@gmail.com>
2021-10-09 22:19:18 +1100

    Add program's note, and delete the test outcomes

--- ROBOT_CHALLENGE/Properties/AssemblyInfo.cs
+++ ROBOT_CHALLENGE/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Robot-Challenge-Extended")]
+[assembly: AssemblyTitle("ROBOT_CHALLENGE")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Robot-Challenge-Extended")]
+[assembly: AssemblyProduct("ROBOT_CHALLENGE")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
